e2a0d8332caa01bd4962064a349566720d1ecdb3
Promo <promo152003@gmail.com>
2023-08-20 10:58:28 +0530

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,3 @@
-
-$(document).ready(function(){
-$('#menu').click(function(){
-$(this).toggleClass('fa-times');
-$('header').toggleClass('toggle');
-});
-$(window).on('scroll load',function(){
-    $('#menu').removeClass('fa-times');
-    $('header').removeClass('toggle');
-
-});
-
-$('a[href*="#"]').on('click',function(e){
-e.preventDefault();
-
-$('html,body').animate({
-    scrollTop: $($(this).attr('href')).offset().top,
-},
-500,
-'linear'
-);
-
-});
-
-});
 function sendMail() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
